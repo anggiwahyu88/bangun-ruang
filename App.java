7a37fc9079c369011b27381966f73e9194cc7ace
fathursn21 <faturrosin333@gmail.com>
2025-02-14 16:11:25 +0700

    Pilihan Trapesium

--- App.java
+++ App.java
@@ -29,27 +29,27 @@ class App {
 
             }
             if (pilihan == 2) {
-                System.out.print("Masukkan Alas Segitiga: ");
+                System.out.print("Masukkan Alas Segitiga : ");
                 int alas = input.nextInt();
-                System.out.print("Masukkan Tinggi Segitiga: ");
+                System.out.print("Masukkan Tinggi Segitiga : ");
                 int tinggi = input.nextInt();
                 var segitiga = new Segitiga(alas, tinggi);
                 segitiga.luas();
                 segitiga.keliling();
             }
             if (pilihan == 3) {
-                System.out.print("Masukkan Diameter Lingkaran: ");
+                System.out.print("Masukkan Diameter Lingkaran : ");
                 int diameter = input.nextInt();
                 var lingkaran = new Lingkaran(diameter);
                 lingkaran.keliling();
                 lingkaran.luas();
             }
             if (pilihan == 4) {
-                System.out.print("Masukkan Panjang Sisi Atas Trapesium");
+                System.out.print("Masukkan Panjang Sisi Atas Trapesium : ");
                 int sisi_atas = input.nextInt();
-                System.out.print("Masukkan Panjang Sisi Bawah Trapesium");
+                System.out.print("Masukkan Panjang Sisi Bawah Trapesium : ");
                 int sisi_bawah = input.nextInt();
-                System.out.print("Masukkan Panjang Sisi Tinggi Trapesium");
+                System.out.print("Masukkan Panjang Sisi Tinggi Trapesium : ");
                 int tinggi = input.nextInt();
                 Trapesium.Output(sisi_atas, sisi_bawah, tinggi, Trapesium.Luas(sisi_atas, sisi_bawah, tinggi));
 
