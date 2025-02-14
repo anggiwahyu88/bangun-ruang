8e5cee1466485064f275b7200505e408773ab0c5
fathursn21 <faturrosin333@gmail.com>
2025-02-14 13:53:35 +0700

    Persegi

--- Persegi.java
+++ Persegi.java
@@ -1,6 +1,17 @@
 public class Persegi {
-    public static int Luas(sisi) {
-        int luas = sisi;
+    
+    public static int Luas(int sisi) {
+        int luas = sisi * sisi;
+        return luas;
+    }
+    public static int Keliling (int sisi) {
+        int keliling = sisi * 4;
+        return keliling;
+    }
+    public static int Panjang_ruas ( int luas) {
+        int panjang_ruas = luas / 4 ;
+        return panjang_ruas;
+
     }
 }
 
